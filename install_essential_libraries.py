e5979647048aa85635238755290f22f90f8e1efc
akallor <ashwinkallor@gmail.com>
2025-06-23 19:43:53 +0530

    Install all the essential python libraries needed to run the pipeline.

---
+++ install_essential_libraries.py
@@ -0,0 +1,8 @@
+#Install Biopython
+!pip install Bio
+
+#Install jax for TPU usage
+!pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+#Install the FAIR ESM models
+!pip install fair-esm
